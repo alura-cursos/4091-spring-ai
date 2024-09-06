83c06e3b1692536d834e50c7ceb8fba119bae911
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-09-06 16:58:03 -0300

    2.3-Prompt Engineering

--- src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
+++ src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
@@ -18,14 +18,28 @@ public class CategorizadorDeProdutosController {
 
     @GetMapping
     public String categorizar(String produto) {
-        var system = "Você é um categorizador de produtos";
+        var system = """
+                Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+               
+                Escolha uma categoria dentra a lista abaixo:
+               
+                1. Higiene pessoal
+                2. Eletronicos
+                3. Esportes
+                4. Outros
+               
+                ###### exemplo de uso:
+               
+                Pergunta: Bola de futebol
+                Resposta: Esportes
+                """;
 
         return this.chatClient.prompt()
                 .system(system)
                 .user(produto)
                 .options(ChatOptionsBuilder
                         .builder()
-                        .withTemperature(0.85f)
+                        .withTemperature(0.8f)
                         .build())
                 .call()
                 .content();
