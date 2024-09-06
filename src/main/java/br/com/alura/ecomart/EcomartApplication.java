7f36283e8cc1fe5d4fcd795cdf5b3e6f29819520
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-09-06 16:24:06 -0300

    Projeto inicial

---
+++ src/main/java/br/com/alura/ecomart/EcomartApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.ecomart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomartApplication.class, args);
+	}
+
+}
